183dca6e44e69b200fa9cad404e68090d7b6afea
Solomon <sedziah1@gmail.com>
2023-11-28 16:44:46 +0000

    Initial Commit

---
+++ script.js
@@ -0,0 +1,46 @@
+// script.js
+document.addEventListener("DOMContentLoaded", function () {
+  const navbarToggle = document.createElement("button");
+  navbarToggle.innerText = "☰";
+  navbarToggle.classList.add("navbar-toggle");
+  const navbar = document.querySelector(".navbar");
+  navbar.insertBefore(navbarToggle, navbar.children[1]);
+
+  navbarToggle.addEventListener("click", function () {
+    const navbarLinks = document.querySelector(".navbar-links");
+    if (navbarLinks.style.display === "block") {
+      navbarLinks.style.display = "none";
+    } else {
+      navbarLinks.style.display = "block";
+    }
+  });
+});
+
+// document.addEventListener("DOMContentLoaded", function () {
+//   let slides = document.querySelectorAll(".slide");
+//   let index = 0;
+
+//   function nextSlide() {
+//     slides[index].classList.remove("active");
+//     index = (index + 1) % slides.length;
+//     slides[index].classList.add("active");
+//   }
+
+//   setInterval(nextSlide, 5000); // Change slide every 8 seconds
+// });
+
+document.addEventListener("DOMContentLoaded", function () {
+  let slides = document.querySelectorAll(".slide");
+  let currentSlide = 0;
+
+  function nextSlide() {
+    slides[currentSlide].classList.remove("active");
+    currentSlide = (currentSlide + 1) % slides.length;
+    slides[currentSlide].classList.add("active");
+  }
+
+  // Change slide every 5 seconds
+  setInterval(nextSlide, 5000);
+});
+
+
