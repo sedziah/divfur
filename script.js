25454541fa7a1a4ae5227014d3281ca03c9bd9b1
Solomon <sedziah1@gmail.com>
2023-12-04 15:09:18 +0000

    mobile responsiveness added for homepage sections

--- script.js
+++ script.js
@@ -21,5 +21,23 @@ $(function() {
   $("#footer-placeholder").load("footer.html");
 });
 
+document.addEventListener("DOMContentLoaded", () => {
+  const navbarToggle = document.getElementById("navbar-toggle");
+  const navbarLinks = document.getElementById("navbar-links");
+
+  navbarToggle.addEventListener("click", () => {
+    navbarLinks.style.right = "0"; // Open the drawer
+  });
+
+  // You can use the same toggle button to close or a separate close button as needed
+  navbarToggle.addEventListener("click", () => {
+    if (navbarLinks.style.right === "0px") {
+      navbarLinks.style.right = "-100%"; // Close the drawer
+    } else {
+      navbarLinks.style.right = "0"; // Open the drawer
+    }
+  });
+});
+
 
 
